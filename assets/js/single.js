a37c78e9c8d95b8250f8f6bd936945d614d66201
JessBot500 <jessica.bergstrom20@outlook.com>
2020-07-05 21:45:13 -0500

    if loop for no issues, for loop with length of issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -7,12 +7,31 @@ var displayWarning = function (repo) {
     limitWarningEl.textContent = "To see more than 30 issues, visit ";
 };
 
+var getRepoIssues = function (repo) {
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+    fetch(apiUrl).then(function (response) {
+        // request successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                // pass response data to dom function
+                displayIssues(data);
+                console.log(data);
+            });
+        }
+        else {
+            alert("There was a problem with your request!");
+        }
+    });
+};
+
 var displayIssues = function (issues) {
 
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
     }
+    issueContainerEl.appendChild(issueEl);
 
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
@@ -42,36 +61,6 @@ var displayIssues = function (issues) {
 
     // append to container
     issueEl.appendChild(typeEl);
-
-};
-
-var getRepoIssues = function (repo) {
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-
-    if (response.ok) {
-        response.json().then(function (data) {
-            displayIssues(data);
-
-            // check if api has paginated issues
-            if (response.header.get("Link")) {
-                console.log("repo has more than 30 issues");
-            }
-        });
-    }
-
-    fetch(apiUrl).then(function (response) {
-        // request was successful
-        if (response.ok) {
-            response.json().then(function (data) {
-                // pass response data to dom function
-                displayIssues(data);
-            });
-        }
-        else {
-            alert("There was a problem with your request!");
-        }
-    });
-    console.log(repo);
 };
 
 getRepoIssues("facebook/react");
